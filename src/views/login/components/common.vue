9242c9d5b2565ea72c6c68fbacedd0451101e9ea
任婷婷 <1542078062@qq.com>
2021-04-14 16:45:45 +0800

    [add]注册，登录，申请试用制作

--- src/views/login/components/common.vue
+++ src/views/login/components/common.vue
@@ -1,81 +1,124 @@
 <template>
   <div class="login">
-    <div class="leftimg"></div>
     <div class="infobox">
-      <div class="logotitle">Hello,欢迎登录Dhunting！</div>
-      <a-tabs default-active-key="1" class="logintabs">
-        <a-tab-pane key="1" tab="账号密码登录" >
-          <loginTel class="ant-tabs-tabpane"></loginTel>
-        </a-tab-pane>
-        <a-tab-pane key="2" tab="验证码登录" >
-         <loginCode class="ant-tabs-tabpane"></loginCode>
-        </a-tab-pane>
-      </a-tabs>
+      <slot name='CommonTitle'></slot>
+      <div class="role">
+        <a-space>
+          <a-button
+            v-for="item in userList"
+            :key="item.id"
+            :type="showBtn === item.id?'primary':''"
+            :class="showBtn === item.id?'primarybtn':'' "
+            @click="btnChange(item.id)">
+            {{item.name}}
+          </a-button>
+        </a-space>
+      </div>
+      <slot name='CommonContent'></slot>
+      <div class="width80" v-if='type !== 3'>
+        <a-checkbox v-decorator="[
+          'remember',
+          {
+            valuePropName: 'checked',
+            initialValue: true,
+          },
+          ]">
+          记住密码
+        </a-checkbox>
+        <a-button type="link" class="loginForgot" v-if='type === 1'>
+          忘记密码？
+        </a-button>
+      </div>
+      <div>
+        <router-link to="/applyUse"  v-if='type !== 3'>
+          <a-button type="link" >申请试用 </a-button>
+        </router-link>
+        <span v-if='type !== 3' class="ft14">
+          或
+        </span>
+        <router-link to="/register" v-if='type !== 2'>
+          <a-button type="link" >立即注册 </a-button>
+        </router-link>
+        <span v-if='type !== 2 && type !== 1' class="ft14">
+          或
+        </span>
+        <router-link to="/login" v-if='type !== 1'>
+          <a-button type="link" >立即登录 </a-button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import loginTel from "./loginTel"
-import loginCode from "./loginCode"
+
 export default {
   name: 'Home',
   components: {
-    loginTel,
-    loginCode
+  },
+  props: {
+    userList: {
+      type: Array
+    },
+    type: {
+      type: Number,
+      require: true
+    },
   },
   data() {
     return {
-
+      showBtn: 1,
     };
   },
   methods: {
+    // 按钮切换
+		btnChange(i){
+			this.showBtn = i
+		}
   },
 }
 </script>
-<style scoped>
+<style lang="less" scoped >
+@import '@/assets/css/main.less'; // 用于覆盖上面定义的变量. 全局变量
   .login{
     width:100%;
     height: 100vh;
-    background:#e9edf8;
-    display: flex;
+    background:#e9edf8 url("../../../assets/img/loginbg.png") no-repeat left center;
+    background-size: 58% 100%;
+    display: flex; 
+    justify-content:flex-end
  }
-  .leftimg{
-    width: 50%;
+.infobox{
+    width: 46%;
     height: 100%;
-    background: url("../../assets/img/loginbg.png") no-repeat left center;
-    background-size: cover;
-  }
- .infobox{
-    width: 50%;
-    height: 100%;
-    background: #FFFFFF url("../../assets/img/loginbottom.png") no-repeat right bottom;
+    background: #FFFFFF url("../../../assets/img/loginbottom.png") no-repeat right bottom;
     background-size: 100px;
     border-radius: 30px;
     padding: 60px 0;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
+    // align-items: center;
+    // justify-content: space-around;
  }
- .logotitle{
-    text-align: center;
-    font-size: 40px;
-    font-family: Lantinghei SC;
-    font-weight: 540;
-    color: #3A54B6;
- }
- .logintabs{
+  @media screen and (max-width: 1200px) and (min-width:768px) {
+    .infobox {
+      width: 60%;
+    }
+}
+ @media screen and (max-width: 768px) {
+    .infobox {
+      width: 100%;
+    }
+}
+.ft14{
+  font-size: 14px;
+}
+.role{
   width: 80%;
-  flex:1;
-  padding-top: 40px;
- }
- .logintabs .ant-tabs-tabpane {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 400px;
-  padding-top: 20px;
+  margin:20px 0;
+  text-align: left;
+}
+.loginForgot{
+  float: right;
 }
 </style>
